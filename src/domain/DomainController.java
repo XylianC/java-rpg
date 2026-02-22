697ab4fb961f1305be73f78e89f35edc254295e1
Xylian <xylian.cocquyt@student.hogent.be>
2026-02-22 19:48:31 +0100

    Created attack repository, and XML loader to automatically load attacks from XML file, enemies will work with this as well. See attackrepository.java

--- src/domain/DomainController.java
+++ src/domain/DomainController.java
@@ -1,5 +1,7 @@
 package domain;
 
+import persistance.AttackRepository;
+
 public class DomainController {
     public DomainController() {
     
--- src/persistance/AttackRepository.java
+++ src/persistance/AttackRepository.java
@@ -5,14 +5,39 @@ import domain.Attack;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class AttackRepository {
     private HashMap<String, Attack> attacks = new HashMap<>();
     
+    public void loadAttacks() throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
     
-    
+        Document doc = db.parse(getClass().getResourceAsStream("attacks.xml"));
+        
+        NodeList attackNodes = doc.getElementsByTagName("attack");
+        
+        for (int i = 0; i < attackNodes.getLength(); i++) {
+            Element attackElement = (Element) attackNodes.item(i);
+            String code = attackElement.getElementsByTagName("code").item(0).getTextContent();
+            String name = attackElement.getElementsByTagName("name").item(0).getTextContent();
+            String element = attackElement.getElementsByTagName("element").item(0).getTextContent();
+            int baseDMG = Integer.parseInt(attackElement.getElementsByTagName("baseDMG").item(0).getTextContent());
+            int baseACC = Integer.parseInt(attackElement.getElementsByTagName("baseACC").item(0).getTextContent());
+            boolean isMagic = Boolean.parseBoolean(attackElement.getElementsByTagName("isMagic").item(0).getTextContent());
+        
+            Attack atkToAdd = new Attack(code, name, element, baseDMG, baseACC, isMagic);
+            attacks.put(code, atkToAdd);
+        }
+    }
 }
+
